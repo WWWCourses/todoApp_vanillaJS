33e9a2c7dba86d4ee717229d904f16fbeef10f84
WWWCourses <ivawebcourses@gmail.com>
2021-01-14 17:05:42 +0200

    update

--- src/todo_restAPI.js
+++ src/todo_restAPI.js
@@ -7,7 +7,7 @@ const displayTodoItemsCount = function() {
 }
 
 const addTodo = function() {
-    // get the input text and make new todo object:
+	// get the input text and make new todo object:
 	const todoText = nodes.addTodoInput.value;
 	const newTodo = {
 		"title": todoText,
@@ -22,16 +22,13 @@ const addTodo = function() {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(newTodo)
-	}).then(response=>{
-		if(response.status === 201){
-			// sync and render todos:
-			todos = [...todos, newTodo];
-			renderTodos();
-		}
 	})
-
-	// update todos count:
-    displayTodoItemsCount();
+	.then(response=> response.json())
+	.then(data=>{
+		// console.dir(data);
+		todos = [...todos, data];
+		renderTodos();
+	})
 
     // clear input text
     nodes.addTodoInput.value = '';
@@ -39,33 +36,25 @@ const addTodo = function() {
     // focus on input for new todo:
     nodes.addTodoInput.focus();
 }
+
 const removeTodo = function (e) {
-	// get id of todo to be removed:
-	let todoID;
-	if(e.target.classList.contains('fa-trash-alt')){
-		todoID = e.target.parentNode.parentNode.dataset.id;
-	}else if( e.target.classList.contains('removeTodo')){
-		todoID = e.target.parentNode.dataset.id;
+	if(e.target.classList.contains("fa-trash-alt")){
+		let todoID = e.target.parentNode.parentNode.dataset.id*1;
+
+		// remove todo:
+		fetch(`${todosAPIroot}/${todoID}`,{
+			method: 'DELETE'
+		})
+		.then(response=>response.json())
+		.then(data =>{
+			// sync and render todos - we can fetch again all todos, or skip the load and sync it localy:
+			todos = todos.filter(todo=> todo.id !== todoID)
+			renderTodos();
+		})
 	}
 
-	// remove todo:
-	fetch(`${todosAPIroot}/${todoID}`,{
-		method: 'DELETE'
-	})
-	.then(response=>{
-		console.log(response);
-		if(response.status === 200){
-			// sync and render todos - we can fetch again all todos, or skip the load and sync it localy:
-			// get the index of todo to be removed and remove it:
-			// console.dir(todos);
-			let idx = todos.findIndex(todo => {
-				(todo.id === todoID) || (console.log(`todo.id: ${todo.id}, todoID: ${todoID}`));
-			});
-			// console.log(`idx: ${idx}`);
-			idx>=0 && todos.splice(idx,1);
-		}
-	})
 }
+
 const completeTodo = function (e) {
 	// HW: implement the function body
 }
@@ -82,7 +71,7 @@ const renderTodos = function(e) {
 			<span class="todoID">${todo.id}.</span>
 			<span ${todo.completed? 'class="completed"' : ''}>${todo.title}</span>
 			<div class="removeTodo"><i class="far fa-trash-alt"></i></div>
-			<div class="editTodo"><i class="fas fa-pencil-alt"></i></div>
+			<div class="editTodo"><i class="fas fa-check-square"></i></div>
 		`;
 
 		li.setAttribute("data-id", todo.id);
@@ -100,7 +89,8 @@ let nodes = {
     'totalItemsCount': document.querySelector('.todoApp .total>.output')
 }
 
-let todosAPIroot = 'http://localhost:3000/todos';
+
+let todosAPIroot = 'http://localhost:3001/todos';
 
 // todos array of todo objects:
 let todos = [];
@@ -112,7 +102,7 @@ window.addEventListener('DOMContentLoaded', event=>{
 	fetch(todosAPIroot)
 		.then(response=>response.json())
 		.then(data => {
-			console.log(`data: ${data}`);
+			console.dir(data);
 			// sync and render todos:
 			todos = [...data];
 			renderTodos();

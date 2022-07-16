3f3cee1dc4e2c4080d4d6ad3a4c84ff583fa4714
WWWCourses <ivawebcourses@gmail.com>
2022-07-16 12:47:47 +0300

    Ready with hardcoded Todos

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Simple Todo App
+
+For labs demos, using React.
---
+++ src/todoApp.js
@@ -0,0 +1,126 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// define variables and functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const displayTodoItemsCount = function displayTodoItemsCount(todos) {
+	let count = todos.length || 0;
+	nodes.totalItemsCount.innerHTML = count;
+}
+
+const renderTodos = function renderTodos(todos) {
+	// clean current todos:
+	nodes.todoItems.innerHTML = '';
+
+	// add todo item at the end
+	todos.forEach( todo => {
+		nodes.todoItems.innerHTML += `
+		<li data-id=${todo.id} class="${todo.completed?'completed':''}">
+			<span class="todoID">${todo.id}.</span>
+			<span>${todo.title}</span>
+			<div class="todo-remove"><i class="far fa-trash-alt"></i></div>
+			<div class="todo-togle-complete"><i class="far ${todo.completed ? 'fa-check-square':'fa-square'}"></i></div>
+		</li>
+		`;
+	})
+
+	// clear input text
+	nodes.addTodoInput.value = '';
+
+	// focus on input for new todo:
+	nodes.addTodoInput.focus();
+
+	displayTodoItemsCount(todos);
+}
+
+const addTodo = function addTodo() {
+	// get the input text
+	const todoText = nodes.addTodoInput.value;
+
+	// create the new todo object
+	const newTodo = {
+		"id": todos[todos.length-1]?.id +1 || 1, // in real app will be done by the server
+		"title": todoText,
+		"completed": false,
+	};
+
+	// change local state
+	todos = [...todos, newTodo];
+
+	// update UI on state change:
+	renderTodos(todos);
+}
+const removeTodo = function removeTodo(todoID) {
+	// change local state:
+	todos = todos.filter(todo=> todo.id !== todoID)
+
+	// update UI on state change:
+	renderTodos(todos);
+}
+
+const toggleComplete = function toggleComplete(todoID) {
+	// console.log(`todoID: ${todoID}`);
+	//get todo object to be patched
+	let todo = todos.filter(todo=>todo.id===todoID)[0];
+
+	// change local state:
+	todo.completed = !todo.completed;
+
+	// update UI on state change:
+	renderTodos(todos);
+}
+
+
+// DOM cache:
+const nodes = {
+	'todoItems': document.querySelector('ul.todo-list-items'),
+	'addTodoInput': document.querySelector('.todo-add>input'),
+	'addTodoBtn': document.querySelector('.todo-add>.todo-add-btn'),
+	'totalItemsCount': document.querySelector('.todo-app .todos-total>.output')
+}
+
+// inint local todos (state)
+let todos = [
+	{
+		id:1,
+		title:'Todo1',
+		completed: false
+	}
+];
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// attach events
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+window.addEventListener('DOMContentLoaded', function (e) {
+	renderTodos(todos)
+});
+
+//// add Todo Item
+// TODO: do not addTodo on empty input
+// on Add button click
+nodes.addTodoBtn.addEventListener('click', function (e) {
+	addTodo();
+});
+// on 'enter' key pressed:
+nodes.addTodoInput.addEventListener('keypress', function(e) {
+	if(e.keyCode === 13){
+		addTodo();
+	}
+})
+
+
+nodes.todoItems.addEventListener('click', function todoItemsClickHandler(e) {
+	console.dir(e.target);
+	if( e.target.classList.contains('fa-trash-alt')){
+		const li = e.target.parentElement.parentElement
+		const id = li.dataset.id*1;
+
+		removeTodo(id)
+	}else if( e.target.classList.contains('fa-check-square')|| e.target.classList.contains('fa-square')){
+		const li = e.target.parentElement.parentElement
+		const id = li.dataset.id*1;
+
+		toggleComplete(id)
+		e.preventDefault()
+	}else{
+		e.preventDefault()
+	}
+})
